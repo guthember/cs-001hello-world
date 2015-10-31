77474b99b19f4a9e89b3abc51edf8846bda91941
guthember <guthember@gmail.com>
2015-10-31 09:24:25 +0100

    First version
    There aren't comments

---
+++ hello-world.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace proba
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
